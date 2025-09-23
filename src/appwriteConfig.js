7c374143583703b3d702f50f1e056ce04e95eb20
Hyawii Hyawii <152699531+HyawiiGithub@users.noreply.github.com>
2025-09-24 00:03:35 +0200

    Update appwriteConfig.js

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -4,6 +4,6 @@ const client = new Client();
 
 client
   .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('YOUR_PROJECT_ID'); // You'll need to set this in your Appwrite project
+  .setProject('68d3133e001e9cdbe2da'); // You'll need to set this in your Appwrite project
 
 export { client };
